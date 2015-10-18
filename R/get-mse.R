cea518f608b1907fe810d25336cd881c3c498b97
Zhenke Wu <zhenkewu@gmail.com>
2015-10-18 16:32:36 -0400

    added metrics for evaluating finite-sample performance; get_metric

--- R/get-mse.R
+++ R/get-mse.R
@@ -1,23 +1,19 @@
 #' Obtain MSE, bias-squared and variance of posterior from nplcm fitted folders.
 #'
 #' The result is equivalent to Euclidean-type calculation after the
-#' compositional vector (e.g., etiologic fraction) is centered-logratio tranformed.
+#' compositional vector (e.g., etiologic fraction) is centered-log-ratio (CLRB) tranformed.
 #' For simulation only.
 #'
 #' @param DIR_NPLCM File path where Bayesian results are stored
-#' @param truth Default is \code{NULL}. True etiologic fraction vector (must sum to 1) used to generate data
-#' @param check.truth Default is \code{FALSE}. If \code{truth} is a non-null vector,
-#' it asks the function to check the user specified truth against the one used
-#' in the actual simulation.
+#' @param truth True etiologic fraction vector (must sum to 1) 
+#' used to generate data
 #' @importFrom robCompositions cenLR
-#' @importFrom coda read.coda
-#' @return a vector of (MSE, bias-squared, variance,truth)
+#' @return a vector of (Integrated Squared Aitchison Distance (ISAD), bias-squared, variance, truth)
 #' @export
 #'
 
-
-get_mse <- function(DIR_NPLCM,truth=NULL,check.truth=FALSE){
-
+get_metric <- function(DIR_NPLCM,truth){
+  use_jags <- is_jags_folder(DIR_NPLCM)
   my.mse <- function(A,B){
     if (nrow(A)!=nrow(B)){
       stop("not equal number of rows!")
@@ -26,10 +22,9 @@ get_mse <- function(DIR_NPLCM,truth=NULL,check.truth=FALSE){
       bb=cenLR(B)$x.clr
       res <- rowSums((aa-bb)^2)
       mean(res)
-
     }
   }
-
+  
   my.bias.sq <- function(A,B){
     if (nrow(A)!=nrow(B)){
       stop("not equal number of rows!")
@@ -39,101 +34,48 @@ get_mse <- function(DIR_NPLCM,truth=NULL,check.truth=FALSE){
       sum((colMeans(aa-bb))^2)
     }
   }
-
+  
   #A = as.data.frame(true_pEti)
   my.var <- function(A){
-
     res <- cenLR(A)$x.clr
     sum(diag(cov(res)))
-
   }
-
-  if (!file.exists(DIR_NPLCM)){
-    stop("==No such folder!==")
-  }else{
-    #read in data from the folder directory:
-    bugs.dat <- dget(paste(DIR_NPLCM,"data.txt",sep="/"))
-    for (bugs.variable.name in names(bugs.dat)) {
-      if (!is.null(dim(bugs.dat[[bugs.variable.name]]))) {
-        dim(bugs.dat[[bugs.variable.name]]) <- rev(dim(bugs.dat[[bugs.variable.name]]))
-        bugs.dat[[bugs.variable.name]] <- aperm(bugs.dat[[bugs.variable.name]])
-      }
-      assign(bugs.variable.name, bugs.dat[[bugs.variable.name]])
-    }
-
-    model_options  <- dget(paste(DIR_NPLCM,"model_options.txt",sep="/"))
-
-    if (is.null(truth)){
-      print("==No truth specified; Looking in the result folder!==")
-      if (!file.exists(paste(DIR_NPLCM,"set_parameter.txt",sep="/"))){
-        stop("==No set_parameter.txt available in result folder to check
-                  equality of specified and used true etiology!==")
-      }else{
-        set_parameter <- dget(paste(DIR_NPLCM,"set_parameter.txt",sep="/"))
-        use.truth <- set_parameter$etiology
-      }
-    }else{
-        if (check.truth){
-          if (!file.exists(paste(DIR_NPLCM,"set_parameter.txt",sep="/"))){
-            stop("==No set_parameter.txt available in result folder to check
-                  equality of specified and used true etiology!==")
-          } else{
-            set_parameter <- dget(paste(DIR_NPLCM,"set_parameter.txt",sep="/"))
-            if (sum(set_parameter$etiology-truth)>0){
-              stop("==Specified true etiology is different than the actual one that
-               generates the simulated data!==")
-            }
-          }
-        }
-      use.truth <- truth
-    }
-    #compatibility checking:
-    if (length(model_options$M_use)!=length(model_options$TPR_prior)){
-      stop("The number of measurement source(s) is different from
-             the number of TPR prior option!
-             Make them equal, and match with order!")
-    }
-
-    #some data preparation:
-    Nd <- bugs.dat$Nd
-    Nu <- bugs.dat$Nu
-
-    Y = c(rep(1,Nd),rep(0,Nu))
-
-    pathogen_list     <- model_options$pathogen_list
-
-    Jfull_BrS         <- length(pathogen_list)
-
-    #reading nplcm outputs:
-    res_nplcm <- read.coda(paste(DIR_NPLCM,"coda1.txt",sep="/"),
-                           paste(DIR_NPLCM,"codaIndex.txt",sep="/"),
-                           quiet=TRUE)
-
-    Jfull <- length(grep("pEti",colnames(res_nplcm)))
-
-    # extract and process some data and posterior samples:
-    SubVarName <- rep(NA,Jfull)
-    for (j in 1:Jfull){
-      SubVarName[j] = paste("pEti","[",j,"]",sep="")
-    }
-
-    #get etiology fraction MCMC samples:
-    pEti_mat   <- as.matrix(res_nplcm[,SubVarName])
-
-
-    true_pEti <- as.data.frame(matrix(use.truth,
-                                      nrow(pEti_mat),ncol(pEti_mat),
-                                      byrow=TRUE))
-    # Aitchison distances:
-    #aDist(true_pEti,pEti_mat)
-    mse <- my.mse(true_pEti,pEti_mat)
-    bias.sq <- my.bias.sq(true_pEti,pEti_mat)
-    vv <- my.var(as.data.frame(pEti_mat))
-
-    res <- list(mse=mse,bias.sq=bias.sq,vv=vv,truth=use.truth)
-    res
+  
+  #read in data from the folder directory:
+  
+  out <- nplcm_read_folder(DIR_NPLCM)
+  model_options <- out$model_options
+  bugs.dat      <- out$bugs.dat
+  res_nplcm     <- out$res_nplcm
+  #some data preparation:
+  Nd <- bugs.dat$Nd
+  Nu <- bugs.dat$Nu
+  
+  Y <-  out$Y
+  
+  cause_list     <- model_options$likelihood$cause_list
+  Jcause         <- length(grep("^pEti\\[",colnames(res_nplcm)))
+  if (Jcause != length(cause_list)){
+    stop("=='model_options' and actual posterior results 'pEti' have different number of causes!==")
   }
-
+  
+  # extract and process some data and posterior samples:
+  SubVarName <- rep(NA,Jcause)
+  for (j in 1:Jcause){
+    SubVarName[j] = paste("pEti","[",j,"]",sep="")
+  }
+  #get etiology fraction MCMC samples:
+  pEti_mat   <- as.matrix(res_nplcm[,SubVarName])
+  true_pEti  <- as.data.frame(t(replicate(nrow(pEti_mat),truth)))
+  
+  # Aitchison distances:
+  # aDist(true_pEti,pEti_mat)
+  mse     <- my.mse(true_pEti,pEti_mat)
+  bias.sq <- my.bias.sq(true_pEti,pEti_mat)
+  vv      <- my.var(as.data.frame(pEti_mat))
+  
+  res <- make_list(mse, bias.sq,vv,truth)
+  res
 }
 
 
