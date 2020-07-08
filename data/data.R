7bee2d24b608c89a8f27569c092cde13a8a86a7e
Irena Chen <irena@umich.edu>
2020-07-08 11:30:26 -0400

    add a data description to fix warnings

---
+++ data/data.R
@@ -0,0 +1,11 @@
+
+#' Simulated dataset that is structured in the format necessary for an nplcm with regression 
+#'
+#' @format A list containing three items 
+#' \describe{
+#'   \item{Mobs}{BrS level measurements}
+#'   \item{Y}{case-control status}
+#'   \item{X}{matrix of covariates; enrollment date and site}
+#'   ...
+#' }
+"nplcm_reg_data"
