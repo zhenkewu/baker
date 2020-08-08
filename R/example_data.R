553074e0f65a015cd4a995c084df48a0466a609d
zhenkewu <zhenkewu@gmail.com>
2020-08-07 21:35:22 -0400

    edited data doc

--- R/example_data.R
+++ R/example_data.R
@@ -9,9 +9,7 @@
 #'   \item{pathogen_type}{category of the hypothetical pathogens, \code{B} for
 #'   bacterium, \code{V} for virus, which are randomly assigned.}
 #' }
-#' @examples
-#' data("pathogen_category_simulation")
-#' str(pathogen_category_simulation)
+#' @usage data("pathogen_category_simulation")
 "pathogen_category_simulation"
 
 
@@ -30,7 +28,5 @@
 #'   DATE (index from 1:300), std_date (standardized DATE), ENRLDATE (actual dates)}
 #' }
 #' 
-#' @examples 
-#' data("data_nplcm_reg")
-#' str(data_nplcm_reg)
+#' @usage data("data_nplcm_reg")
 "data_nplcm_reg"
