cea9cd7355b19656c21bdf5e498a682596062f9d
zhenkewu <zhenkewu@gmail.com>
2022-01-29 22:40:44 -0500

    remove sort_data_frame; resubmitted to CRAN

--- R/utils.R
+++ R/utils.R
@@ -282,69 +282,6 @@ unfactor <- function(f) {
 }
 
 
-#' Order rows of data frame according to variable combinations.
-#'
-#'@param form A Formula. See the example below; 
-#' use `+` for ascending, `-` for descending
-#'@param dat Data frame to be ordered.
-#'
-#'@return Ordered data frame.
-#'@examples
-#' df <- as.data.frame(list(x1=c(2,3,1),x2=c(3,4,6)))
-#' sort_data_frame(~-x1+x2,df)
-#'
-#'@export
-sort_data_frame <- function(form,dat) {
-  # Author: Kevin Wright
-  # http://tolstoy.newcastle.edu.au/R/help/04/09/4300.html
-  # Some ideas from Andy Liaw
-  # http://tolstoy.newcastle.edu.au/R/help/04/07/1076.html
-  # Use + for ascending, - for descending.
-  # Sorting is left to right in the formula
-  # Usage is either of the following:
-  # sort.data.frame(~Block-Variety,Oats)
-  # sort.data.frame(Oats,~-Variety+Block)
-  
-  # If dat is the formula, then switch form and dat
-  if (inherits(dat,"formula")) {
-    f = dat
-    dat = form
-    form = f
-  }
-  if (form[[1]] != "~") {
-    stop("Formula must be one-sided.")
-  }
-  # Make the formula into character and remove spaces
-  formc <- as.character(form[2])
-  formc <- gsub(" ","",formc)
-  # If the first character is not + or -, add +
-  if (!is.element(substring(formc,1,1),c("+","-"))) {
-    formc <- paste("+",formc,sep = "")
-  }
-  # Extract the variables from the formula
-  vars <- unlist(strsplit(formc, "[\\+\\-]"))
-  vars <- vars[vars != ""] # Remove spurious "" terms
-  # Build a list of arguments to pass to "order" function
-  calllist <- list()
-  pos = 1 # Position of + or -
-  for (i in 1:length(vars)) {
-    varsign <- substring(formc,pos,pos)
-    pos <- pos + 1 + nchar(vars[i])
-    if (is.factor(dat[,vars[i]])) {
-      if (varsign == "-")
-        calllist[[i]] <- -rank(dat[,vars[i]])
-      else
-        calllist[[i]] <- rank(dat[,vars[i]])
-    }
-    else {
-      if (varsign == "-")
-        calllist[[i]] <- -dat[,vars[i]]
-      else
-        calllist[[i]] <- dat[,vars[i]]
-    }
-  }
-  dat[do.call("order",calllist),]
-}
 
 #' Reorder the measurement dimensions to match the order for display
 #'
